3e645870b6e2644ecfd57ae05978ae8ea0d0d4cc
eo9on9 <eo9on9@gmail.com>
2025-07-14 15:21:33 +0900

    Set tailwindcss

---
+++ packages/ui/src/components/Card.tsx
@@ -0,0 +1,3 @@
+export const Card = () => {
+  return <div className="rounded-lg shadow-md p-4 bg-cyan-400">Card</div>
+}
---
+++ packages/ui/src/components/Title.tsx
@@ -0,0 +1,5 @@
+export const Title = () => {
+  return (
+    <h1 className="text-3xl font-bold underline bg-amber-400">Hello world!</h1>
+  )
+}
