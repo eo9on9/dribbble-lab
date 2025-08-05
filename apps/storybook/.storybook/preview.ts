652a6fba45341a4ce6cc2d28cc97ca8746b3fa77
eo9on9 <eo9on9@gmail.com>
2025-08-05 15:17:12 +0900

    Chore: set stories

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,14 +1,16 @@
+import '@repo/ui/style.css'
+
 import type { Preview } from '@storybook/react-vite'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
