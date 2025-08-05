b22623ab5cb7d3caa099283db232c3cc9d0e2c14
eo9on9 <eo9on9@gmail.com>
2025-08-05 14:04:07 +0900

    Chore: Set lint

--- apps/storybook/eslint.config.js
+++ apps/storybook/eslint.config.js
@@ -1,26 +1,4 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import { config } from '@repo/eslint-config/react-internal'
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-], storybook.configs["flat/recommended"]);
+/** @type {import("eslint").Linter.Config} */
+export default config
