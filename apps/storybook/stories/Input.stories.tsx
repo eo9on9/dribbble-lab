19e4a82450985ddd0eef01f020c417435fb52cbd
eo9on9 <eo9on9@gmail.com>
2025-08-07 08:56:25 +0900

    Docs: Input stories

---
+++ apps/storybook/stories/Input.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { SearchIcon } from '@repo/ui/icons'
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Input } from '@repo/ui/components/Input'
+import { fn } from 'storybook/test'
+
+const meta = {
+  title: 'Example/Input',
+  component: Input,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    placeholder: 'Please enter your name',
+    onChange: fn(),
+    prefix: '',
+  },
+  argTypes: {
+    icon: {
+      control: false,
+      description: 'ReactNode',
+    },
+  },
+} satisfies Meta<typeof Input>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const WithPrefix: Story = {
+  args: {
+    prefix: '#',
+    defaultValue: 'FFFFFF',
+  },
+}
+
+export const WithIcon: Story = {
+  render: args => <Input {...args} icon={<SearchIcon />} />,
+}
